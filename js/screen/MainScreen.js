24de3bf5f69713f569e848fb3a8d4fe6832861de
Aemilia <acb3454@rit.edu>
2022-02-27 00:35:30 -0500

    button distribution

--- js/screen/MainScreen.js
+++ js/screen/MainScreen.js
@@ -16,10 +16,10 @@ export default class MainScreen extends Screen
         const animalimage = await Images[name + hat]
         ctx.drawImage(animalimage, (screen.width/2)-(animalimage.width), 1300);
         ctx.drawImage(await GoalsIcon, 30, 1740)
-        ctx.drawImage(await PetIcon, 230, 1740)
+        ctx.drawImage(await PetIcon, 280, 1740)
         console.log(ShopIcon + "is here")
-        ctx.drawImage(await ShopIcon, 400, 1740)
+        ctx.drawImage(await ShopIcon, 580, 1740)
         console.log(ResourcesIcon + "is here")
-        ctx.drawImage(await ResourcesIcon, 1680, 1740)
+        ctx.drawImage(await ResourcesIcon, 870, 1740)
     }
 }
