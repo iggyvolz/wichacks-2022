a70ec32720fee308adc8bb5d64d41c2ffa4a4deb
Aemilia <acb3454@rit.edu>
2022-02-27 07:11:32 -0500

    resource fixed but not

--- js/screen/ResourcesScreen.js
+++ js/screen/ResourcesScreen.js
@@ -1,12 +1,14 @@
 import Screen from "./Screen.js";
 import Gamestate from "../GameState.js";
 import Images, { BackgroundYard, GoalsIcon, MenuIcon, PetIcon, ResourcesIcon, ShopIcon, GoalAdd } from "../../Images/Images.js";
+import Canvas from "../Canvas.js";
 export default class ResourcesScreen extends Screen
 {
     /**
      * @param {Gamestate} gs
      * @param {CanvasRenderingContext2D} ctx 
      */
+
      async draw(gs, ctx){
         ctx.drawImage(await GoalAdd, 0, 0);
     }
