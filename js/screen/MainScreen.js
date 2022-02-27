dc644ae9bfd79c42efe86d2812ef39962a98676f
Aemilia <acb3454@rit.edu>
2022-02-27 06:14:20 -0500

    got rid of unnecessary menu buttons

--- js/screen/MainScreen.js
+++ js/screen/MainScreen.js
@@ -15,13 +15,13 @@ export default class MainScreen extends Screen
         console.log(name + hat)
         const animalimage = await Images[name + hat]
         ctx.drawImage(animalimage, (screen.width/2)-(animalimage.width), (screen.height + 300));
-        ctx.drawImage(await GoalsIcon, 30, 1740)
-        ctx.drawImage(await PetIcon, 280, 1740)
-        console.log(ShopIcon + "is here")
-        ctx.drawImage(await ShopIcon, 580, 1740)
-        console.log(ResourcesIcon + "is here")
-        ctx.drawImage(await ResourcesIcon, 870, 1740)
-        console.log(MenuIcon)
+        //ctx.drawImage(await GoalsIcon, 30, 1740)
+        //ctx.drawImage(await PetIcon, 280, 1740)
+        //console.log(ShopIcon + "is here")
+        //ctx.drawImage(await ShopIcon, 580, 1740)
+        //console.log(ResourcesIcon + "is here")
+        //ctx.drawImage(await ResourcesIcon, 870, 1740)
+        //console.log(MenuIcon)
         //ctx.drawImage(await MenuIcon, 30, 30)
     }
 }
