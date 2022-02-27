84c33bd7e95a865200cd96fa610d5e2399db0280
Aemilia <acb3454@rit.edu>
2022-02-27 05:45:45 -0500

    correct screens

--- js/screen/ShopScreen.js
+++ js/screen/ShopScreen.js
@@ -1,6 +1,6 @@
 import Screen from "./Screen.js";
 import Gamestate from "../GameState.js";
-import Images, { BackgroundYard, GoalsIcon, MenuIcon, PetIcon, ResourcesIcon, ShopIcon, GoalAdd } from "../../Images/Images.js";
+import Images, { BackgroundYard, GoalsIcon, MenuIcon, PetIcon, ResourcesIcon, ShopIcon, Goal } from "../../Images/Images.js";
 export default class GoalAddScreen extends Screen
 {
     /**
@@ -8,7 +8,7 @@ export default class GoalAddScreen extends Screen
      * @param {CanvasRenderingContext2D} ctx 
      */
      async draw(gs,ctx){
-        ctx.drawImage(await GoalAdd, 0, 0);
+        ctx.drawImage(await Goal, 0, 0);
         console.log(MenuIcon)
         ctx.drawImage(await MenuIcon, 30, 30)
     }
