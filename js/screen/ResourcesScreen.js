c88bf47a21b6adae8388ad8f401ae516aeb51f03
Aemilia <acb3454@rit.edu>
2022-02-27 02:36:55 -0500

    resources screen

---
+++ js/screen/ResourcesScreen.js
@@ -0,0 +1,15 @@
+import Screen from "./Screen.js";
+import Gamestate from "../GameState.js";
+import Images, { BackgroundYard, GoalsIcon, MenuIcon, PetIcon, ResourcesIcon, ShopIcon } from "../../Images/Images.js";
+export default class ResourcesScreen extends Screen
+{
+    /**
+     * @param {Gamestate} gs
+     * @param {CanvasRenderingContext2D} ctx 
+     */
+     async draw(gs,ctx){
+        ctx.drawImage(await GoalAdd, 0, 0);
+        console.log(MenuIcon)
+        ctx.drawImage(await MenuIcon, 30, 30)
+    }
+}
